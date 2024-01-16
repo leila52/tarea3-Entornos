ca1152b61c32702eb914b8e4d666707407345ce2
ainhoa32 <ainhoa32alarcon@gmail.com>
2024-01-16 12:18:20 +0100

    Segundo commit área y perímetro circunferencia

--- entornosTarea3/src/entornosTarea3/ejercicio.java
+++ entornosTarea3/src/entornosTarea3/ejercicio.java
@@ -12,10 +12,27 @@ public class ejercicio {
 		System.out.println("Introduce otro base: ");
 		int base = entrada.nextInt();
 		System.out.println("el area del triangulo es : "+calcularArea(base,altura));
+		
+		//área y perímetro del círculo
+		System.out.println("Introduce el radio de la circunferencia: ");
+		int radio = entrada.nextInt();
+		System.out.println("el area de la circunferencia es: "+calaACirc(radio));
+		System.out.println("el perimetro de la circunferencia es : "+perimetroACirc(radio));
+		
+	
 	}
 	//funcion de triangulo
 	public static double calcularArea(int base, int altura) {
 		return base * altura / 2;
-		}
+
+	//Alumno 2 calcula el área y el perímetro del círculo
+    }public static double calaACirc(int radio) {
+		double area = 3.14*(radio^2);
+		return area;
+	}public static double perimetroACirc(int radio) {
+		double perimetro=2*(3.14*radio);
+		return perimetro;
+		
+	}
 
 }
