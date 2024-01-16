0de2c1723ff143751299357d3979694b7c4b8cd7
ainhoa32 <ainhoa32alarcon@gmail.com>
2024-01-16 12:49:24 +0100

    sexto commit

--- entornosTarea3/src/entornosTarea3/ejercicio.java
+++ entornosTarea3/src/entornosTarea3/ejercicio.java
@@ -59,6 +59,7 @@ public class ejercicio {
 	}public static double areaRectangulo(int lado1rec,int lado2rec) {
 		return ((lado1rec*2) + (lado2rec*2));
 		
+	//que pasa
 	}public static double areaCubo(int ladoCubo) {
 		return (ladoCubo*ladoCubo);
 	}
